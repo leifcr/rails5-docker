86e4def45862990528aa9330b49b010c908ad63d
Leif Ringstad <leif@symbolia.com>
2017-06-20 11:00:45 +0200

    Add dev build with dev dependencies

--- update.sh
+++ update.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
-bundle install && yarn install && git commit -a -m 'Update gems and yarn'
+bundle install && yarn install
+cp Gemfile ./dev
+cp Gemfile.lock ./dev
+cp yarn.lock ./dev
+cp package.json ./dev
+git commit -a -m 'Update gems and yarn'
