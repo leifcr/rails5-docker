17a93160c6f40cda42909ae26aaf8ece642e6e0a
Leif Ringstad <leif@symbolia.com>
2017-11-06 12:25:50 +0100

    Update update script

--- update.sh
+++ update.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
+cp package.json dev/
+cp package.json s2i/
+cp package.json s2i-passenger/
+cp package.json s2i-passenger-standalone/
+cp yarn.lock dev/
+cp yarn.lock s2i/
+cp yarn.lock s2i-passenger/
+cp yarn.lock s2i-passenger-standalone/
+cp Gemfile* dev/
 cp Gemfile* s2i/
 cp Gemfile* s2i-passenger/
 cp Gemfile* s2i-passenger-standalone/
-git commit -a -m 'Update gems'
+git commit -a -m 'Update gems and yarn'
