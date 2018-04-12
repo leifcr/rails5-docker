d28a203d2b4c49d58d45f1d31e3dd2b12f57ee16
Leif Ringstad <leif@symbolia.com>
2018-04-12 15:48:59 +0200

    add version without entrypoint

---
+++ no-entry-point/docker-entrypoint.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set +e
+
+# Check if env is set
+if [ ! -z ${SSH_AUTH_SOCK+x} ]; then
+  # echo 'Loading ssh-agent if not already loaded'
+  ssh-add -l &>/dev/null
+  if [ "$?" == 2 ]; then
+    echo "starting ssh agent"
+    # Check if the pidfile exists and the agent is unconnectable
+    if [ -e "$SSH_AUTH_SOCK" ]; then
+      rm -f $SSH_AUTH_SOCK
+    fi
+    eval "ssh-agent -a $SSH_AUTH_SOCK" >/dev/null
+  fi
+fi
+
+set -e
+case "$1" in
+        annotate|cap|capify|cucumber|foodcritic|guard|irb|jekyll|kitchen|knife)
+        command="bundle exec $@";;
+        middleman|nanoc|pry|puma|rackup|rainbows|rails|rake|rspec|shotgun|sidekiq|spec)
+        command="bundle exec $@";;
+        spork|spring|strainer|tailor|taps|thin|thor|unicorn|unicorn_rails)
+        command="bundle exec $@";;
+        *)
+        command="$@";;
+esac
+exec ${command}
