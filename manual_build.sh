017d7c5a2ba151e8324dcf0e40ec497ae67995fd
Leif Ringstad <leif@symbolia.com>
2019-03-07 13:48:54 +0100

    Remove s2i local build

--- manual_build.sh
+++ manual_build.sh
@@ -3,10 +3,7 @@ cd dev
 docker build . -t leifcr/rails5:latest-dev
 cd ..
 docker build . -t leifcr/rails5:latest
-cd s2i-passenger-standalone
-docker build . -t leifcr/rails5-passenger-standalone:latest
 cd ..
 echo "-------------"
 echo "DONE!"
 echo "-------------"
-
